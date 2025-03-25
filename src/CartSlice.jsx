ae48c182d964f88f0397a65b4e3b31d20218e9b0
lincharles123 <a799802224@gmail.com>
2025-03-25 22:08:51 +0100

    completed

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,19 +1,33 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { act } from 'react';
 
 export const CartSlice = createSlice({
   name: 'cart',
   initialState: {
     items: [], // Initialize items as an empty array
+    addedToCart: {},
   },
   reducers: {
     addItem: (state, action) => {
-    
+      const { name, image, cost } = action.payload;
+      const existingItem = state.items.find(item => item.name === name);
+      if (existingItem) {
+        existingItem.quantity++;
+      } else {
+        state.items.push({ name, image, cost, quantity: 1 });
+        state.addedToCart[name] = true;
+      }
     },
     removeItem: (state, action) => {
+      state.items = state.items.filter(item => item.name !== action.payload.name);
+      delete state.addedToCart[action.payload.name];
     },
     updateQuantity: (state, action) => {
-
-    
+      const { name, quantity } = action.payload;
+      const existingItem = state.items.find(item => item.name === name);
+      if (existingItem) {
+        existingItem.quantity = quantity;
+      }
     },
   },
 });
